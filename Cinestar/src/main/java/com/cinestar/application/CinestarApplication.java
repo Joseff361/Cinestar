2b572f3133de9a350fb81e8ad0674415cd7b8fdb
KattyGutierrez <katugutierrez17@gmail.com>
2020-09-04 12:53:48 -0500

    Commit inicial

---
+++ Cinestar/src/main/java/com/cinestar/application/CinestarApplication.java
@@ -0,0 +1,13 @@
+package com.cinestar.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinestarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinestarApplication.class, args);
+	}
+
+}
---
+++ Cinestar/src/test/java/com/cinestar/application/CinestarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cinestar.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinestarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
